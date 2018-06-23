5f61b36311b3eabfb011b27ce156b3231eae9e8a
Sergey Poluyan <svpoluyan@gmail.com>
2018-06-24 00:16:59 +0300

    linear interpolation

--- quantilefun.r
+++ quantilefun.r
@@ -34,3 +34,21 @@ title(main="cdf")
 
 cdf <- c(0, cdf)
 
+n = 1000 # number of sample
+random = NULL
+sample = NULL
+for(i in 1:n)
+{
+  rnd <- runif(1, 0, 1)
+
+  # linear interpolation
+  index <- which(cdf == min(cdf[cdf>rnd])) - 1
+  x0 <- x[index]
+  y0 <- cdf[index]
+  x1 <- x[index + 1]
+  y1 <- cdf[index + 1]
+  fx <- x0 + (rnd - y0) * (x1 - x0) / (y1 - y0)
+
+  random = c(random,  rnd)
+  sample = c(sample,  fx)
+}
